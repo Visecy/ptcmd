499ede5ca6c3d766662357e22d816556ce0afb62
ovizro <ovizro@visecy.org>
2025-03-31 20:41:14 +0800

    ci(test): update workflow and expose library components
    - Add 'build' package to GitHub Actions workflow
    - Implement build step before coverage measurement
    - Update __init__.py to expose key components of the library

--- src/ptcmd/__init__.py
+++ src/ptcmd/__init__.py
@@ -4,3 +4,18 @@ A modern cmd library based on prompt_toolkit.
 """
 
 from .version import __version__
+from .argument import Arg, Argument
+from .command import Command
+from .core import BaseCmd, Cmd
+from .decorators import auto_argument
+
+
+__all__ = [
+    "Arg",
+    "Argument",
+    "Command",
+    "BaseCmd",
+    "Cmd",
+    "auto_argument",
+    "__version__",
+]
